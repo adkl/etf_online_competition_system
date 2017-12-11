4c04127ad7e15692ec59b428fb716196045b3aeb
United4ever <eceric31@gmail.com>
2017-12-11 13:56:55 +0100

    Dash final

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -17,7 +17,17 @@ export class DashboardComponent implements OnInit {
 
     ngOnInit() {
         //this.getTests();
-        this.tests.push(new Test(1, "12.01.2018 14:00:00", 30));
+        this.tests.push(new Test(1, "12.01.2018 14:00", 30));
+        this.tests.push(new Test(2, "13.01.2018 10:00", 35));
+        this.tests.push(new Test(3, "14.01.2018 14:30", 60));
+        this.tests.push(new Test(4, "15.01.2018 14:00", 30));
+        this.tests.push(new Test(5, "16.01.2018 08:00", 120));
+        this.tests.push(new Test(6, "17.01.2018 09:30", 30));
+        this.tests.push(new Test(7, "18.01.2018 14:30", 120));
+        this.tests.push(new Test(8, "19.01.2018 16:00", 300));
+        this.tests.push(new Test(9, "20.01.2018 15:00", 360));
+        this.tests.push(new Test(10, "21.01.2018 15:30", 360));
+        this.tests.push(new Test(11, "22.01.2018 14:00", 60));
     }
 
     getTests() {
