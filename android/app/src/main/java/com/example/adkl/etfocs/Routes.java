36b63587f0db54e10f90bccbaa7a8bbb7a5011dd
Adnan Alibegovic <aalibegovi2@etf.unsa.ba>
2018-01-14 00:09:16 +0100

    movs

--- android/app/src/main/java/com/example/adkl/etfocs/Routes.java
+++ android/app/src/main/java/com/example/adkl/etfocs/Routes.java
@@ -6,7 +6,7 @@ package com.example.adkl.etfocs;
 
 public class Routes {
     public static String BASE_URL(String ip_address) {
-        return "http://" + "192.168.0.14" + ":8000";
+        return "http://" + "192.168.0.11" + ":8000";
     }
     public final static String LOGIN_ROUTE = "/api-token-auth/";
     public final static String SCHEDULED_TESTS = "/api/scheduled-tests/available-tests/";
